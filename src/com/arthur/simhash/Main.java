d0c5855df73176a611f051c3f322847eada59625
Arthur King <arthur.dut@gmail.com>
2013-10-14 22:12:26 +0800

    commit

---
+++ src/com/arthur/simhash/Main.java
@@ -0,0 +1,24 @@
+package com.arthur.simhash;
+
+public class Main {
+
+	
+	public static void main(String[] argv){
+		String str1 = "this is string1";
+		String str2 = "this is string2";
+		
+		SimHash sim = new SimHash();
+		//compare 6 and 8 for test.
+		sim.compareString(String.valueOf(6), String.valueOf(8));
+		System.out.println();
+		
+		//compare str1 and str2 for test.
+		sim.compareString(str1, str2);
+		
+		
+	}
+
+
+	
+	
+}
